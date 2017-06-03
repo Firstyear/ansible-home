5907ba58af16821ed0942eb312c08d6a09638889
William Brown <william@blackhats.net.au>
2017-06-04 09:36:40 +1000

    Improve nextcloud config

--- roles/bh-libvirt/files/atomic-buildscripts/install-atomic-vm.sh
+++ roles/bh-libvirt/files/atomic-buildscripts/install-atomic-vm.sh
@@ -5,7 +5,7 @@ then
     exit 1
 fi
 
-DISKSIZE=40
+DISKSIZE=80
 MIRROR=ostree.net.blackhats.net.au
 KSNAME=${1}.cfg
 KSPATH=/root/vmscripts/ks/${KSNAME}
@@ -14,7 +14,7 @@ CENTOS_VERSION=7
 
 virt-install --connect=qemu:///system -n $1 \
     --os-variant=rhel7 \
-    --ram=2048 --vcpus=1 --security type=dynamic \
+    --ram=4096 --vcpus=1 --security type=dynamic \
     --serial pty \
     --disk path=/var/lib/exports/t1/def_t1_nfs_sas/${1}.img,sparse=true,format=raw,bus=virtio,size=${DISKSIZE} \
     --location=http://${MIRROR}/pub/centos/7/os/x86_64/ \
