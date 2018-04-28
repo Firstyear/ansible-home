189a7bd75a2a915c2fc7cae3716326f14df7b011
William Brown <william@blackhats.net.au>
2018-04-28 11:03:12 +1000

    Update atomic to use AD

--- containers/radiusd/install.sh
+++ containers/radiusd/install.sh
@@ -5,6 +5,7 @@ IMAGE=${IMAGE:=registry.blackhats.net.au/radiusd}
 
 # Make sure our paths exist
 mkdir -p /host/etc/${NAME}
+# ExecStart=/usr/bin/docker run --rm -v /etc/${NAME}:/etc/raddb -p 1812:1812 -p 1813:1813 --name=${NAME} ${IMAGE}
 
 cat > /host/etc/systemd/system/${NAME}.service << DEVEOF
 [Unit]
@@ -14,7 +15,7 @@ Requires=docker.service
 
 [Service]
 ExecStartPre=-/usr/bin/docker rm ${NAME}
-ExecStart=/usr/bin/docker run --rm -v /etc/${NAME}:/etc/raddb -p 1812:1812 -p 1813:1813 --name=${NAME} ${IMAGE}
+ExecStart=/usr/bin/docker run --rm -v /etc/${NAME}:/etc/raddb --network=host --name=${NAME} ${IMAGE}
 ExecStop=/usr/bin/docker stop -t 60 ${NAME}
 
 [Install]
