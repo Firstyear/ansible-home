405eb32de427b6c68703b75738aa782942582c1d
William Brown <william@blackhats.net.au>
2019-04-01 19:00:02 +1000

    Update

--- roles/bh-rdiff-backup/templates/backup.sh
+++ roles/bh-rdiff-backup/templates/backup.sh
@@ -1,7 +1,7 @@
 {% for item in groups['DailySet1'] %}
 echo ---- START {{ item }} ----
 mkdir -p /home/backup/{{ item }}
-rdiff-backup --ssh-no-compression --print-statistics --include-globbing-filelist /home/backup/include-list "root@{{ item }}::/" /home/backup/{{ item }}/ ;true
+rdiff-backup --ssh-no-compression --print-statistics --include-globbing-filelist /home/backup/include-list "root@{{ item }}::/" /home/backup/{{ item }}/ && \
 rdiff-backup --force --remove-older-than 1W /home/backup/{{ item }}/ ;true
 echo ---- END {{ item }} ----
 
