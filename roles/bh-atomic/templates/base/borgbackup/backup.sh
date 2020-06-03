610364f31a00fe32b82a7b30616131f01b3f50b1
William Brown <william@blackhats.net.au>
2020-06-03 13:53:37 +1000

    add borgbase

--- roles/bh-atomic/templates/base/borgbackup/backup.sh
+++ roles/bh-atomic/templates/base/borgbackup/backup.sh
@@ -32,4 +32,30 @@ borg prune                          \
     --show-rc                       \
     --keep-within 7d
 
+export BORG_REPO={{ borgbasename }}@{{ borgbasename }}.repo.borgbase.com:repo
+export BORG_PASSPHRASE={{ borgbasepw }}
+
+borg create                         \
+    --verbose                       \
+    --filter AME                    \
+    --list                          \
+    --stats                         \
+    --show-rc                       \
+    --exclude-caches                \
+    --exclude '/home/*/.cache/*'    \
+    --exclude '/var/cache/*'        \
+    --exclude '/var/tmp/*'          \
+    --exclude '/.snapshots'         \
+    --exclude '/bin' \
+    --exclude '/usr' \
+    --exclude '/boot' \
+    --exclude '/dev' \
+    --exclude '/data' \
+    --exclude '/proc' \
+    --exclude '/sbin' \
+    ::'{hostname}-{now}'            \
+    /etc                            \
+    /home                           \
+    /root                           \
+    /var/lib/docker/volumes/
 
