af2cad795842a694258e3dc79ee7b286d5ebbf1c
William Brown <william@blackhats.net.au>
2020-06-17 19:23:41 +1000

    add kanidm conf

--- containers/haproxy/init.py
+++ containers/haproxy/init.py
@@ -12,6 +12,12 @@ def do_main():
     hostname = os.environ.get('HAPROXY_HOSTNAME', 'localhost')
     linode_key = os.environ.get('HAPROXY_LINODE_KEY', None)
 
+    raw_target_port = os.environ.get('HAPROXY_RAW_TARGET_PORT', None)
+    raw_listen_port = os.environ.get('HAPROXY_RAW_LISTEN_PORT', None)
+
+    extra = ''
+
+
     if linode_key is not None:
         # Given the linode_key, attempt a certbot setup/renew
         with open("/tmp/linode.ini", 'w') as f:
@@ -63,17 +69,30 @@ dns_linode_version =
         with open(f'/etc/{cpath}/live/{hostname}/privkey.pem', 'r') as f:
             k.write(f.read())
 
+    if raw_target_port and raw_listen_port:
+        extra = f"""
+frontend service_front
+  bind 0.0.0.0:{raw_listen_port} ssl crt /etc/CERTPATH/live/HOSTNAME/bundle.pem
+  mode tcp
+  default_backend service
+
+backend service
+  mode tcp
+  server s1 TARGET_HOST:{raw_target_port}
+"""
+
     lines = None
     with open('/etc/haproxy/haproxy-template.cfg', 'r') as f:
         lines = f.readlines()
 
     with open('/etc/haproxy/haproxy.cfg', 'w') as f:
         for l in lines:
-            l1 = l.replace('TARGET_HOST', target_host)
-            l2 = l1.replace('TARGET_PORT', target_port)
-            l3 = l2.replace('HOSTNAME', hostname)
-            l4 = l3.replace('CERTPATH', cpath)
-            f.write(l4)
+            l1 = l.replace('EXTRA', extra)
+            l2 = l1.replace('TARGET_HOST', target_host)
+            l3 = l2.replace('TARGET_PORT', target_port)
+            l4 = l3.replace('HOSTNAME', hostname)
+            l5 = l4.replace('CERTPATH', cpath)
+            f.write(l5)
 
     subprocess.call([
         'cat',
